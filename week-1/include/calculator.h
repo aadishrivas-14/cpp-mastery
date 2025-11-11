131a2123994bafb49fec43c5b82f5e41dceb0291
Aditya Shrivas <aadishrivas@gmail.com>
2025-11-12 01:27:16 +0530

    Day-1 done

--- week-1/include/calculator.h
+++ week-1/include/calculator.h
@@ -10,6 +10,22 @@
  * - divide(a, b) - Return quotient
  * 
  * All methods should be const and take double parameters
+ * 
+ * 
+The compiler doesn't know:
+• What is Calculator?
+• Is add a method of this class?
+• What are the parameter types?
+• Is it const?
+
+## The Header is Essential Because:
+
+1. Declares the class structure - tells compiler what Calculator is
+2. Declares method signatures - tells compiler what methods exist
+3. Shared between files - both .cpp and test files need it
+4. Enables separate compilation - files can be compiled independently
+
+## Bottom Line:
  */
 
 class Calculator {
--- week-1/src/calculator.cpp
+++ week-1/src/calculator.cpp
@@ -1,24 +1,18 @@
 #include "calculator.h"
 
 // TODO: Implement calculator methods
-
 double Calculator::add(double a, double b) const {
-  // TODO: Return sum of a and b
-  return 0.0;
+  return a + b;
 }
 
 double Calculator::subtract(double a, double b) const {
-  // TODO: Return difference of a and b
-  return 0.0;
+  return a - b;
 }
 
 double Calculator::multiply(double a, double b) const {
-  // TODO: Return product of a and b
-  return 0.0;
+  return a * b;
 }
 
 double Calculator::divide(double a, double b) const {
-  // TODO: Return quotient of a and b
-  // Consider: What if b is zero?
-  return 0.0;
+  return a / b;
 }
