7bc8e90f7a752b97089558a6becb726a0b488582
Aditya Shrivas <aadishrivas@gmail.com>
2025-11-12 14:00:40 +0530

    Day-4 done

--- week-1/src/animal.cpp
+++ week-1/src/animal.cpp
@@ -2,12 +2,10 @@
 
 // TODO: Implement Dog::speak()
 std::string Dog::speak() const {
-  // TODO: Return "Woof"
-  return "";
+  return "Woof";
 }
 
 // TODO: Implement Cat::speak()
 std::string Cat::speak() const {
-  // TODO: Return "Meow"
-  return "";
+  return "Meow";
 }
